f8cdee20cf36356fe1bb9b256f722b906c6181d4
TEJAS-3008 <186479992+TEJAS-3008@users.noreply.github.com>
2026-06-01 22:26:29 +0530

    better version

--- 80.removedupfromsortedarr2.cpp
+++ 80.removedupfromsortedarr2.cpp
@@ -4,6 +4,31 @@
 #include<map>
 using namespace std;
 
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.size() <= 2)
+            return nums.size();
+        int k = 2;
+
+        for(int i = 2; i < nums.size(); i++) {
+            if(nums[i] != nums[k - 2]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+};
+
+//TIME COMPLEXITY O(n)
+//space complexity O(1)
+
+
+
+
+/* PREVIOUS SOLUTION
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
@@ -20,7 +45,9 @@ public:
     }
 };
 
+
 //time complexity O(nlogn) n  for the loop, log(n) for maping
 //space compelxity O(n)
 //two pointer approach might take O(n) time complexity?
+*/
 
