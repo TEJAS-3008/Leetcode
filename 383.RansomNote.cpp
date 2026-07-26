d0747654e8d659f91e0cb976e70ab7e81c90ae96
TEJAS-3008 <186479992+TEJAS-3008@users.noreply.github.com>
2026-07-26 16:46:23 +0530

    383

---
+++ 383.RansomNote.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+  bool canConstruct(string ransomNote, string magazine) {
+    unordered_map<char, int> freq;
+
+    for (char c : magazine) {
+      freq[c]++;
+    }
+
+    for (char c : ransomNote) {
+      freq[c]--;
+
+      if (freq[c] < 0) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+};
