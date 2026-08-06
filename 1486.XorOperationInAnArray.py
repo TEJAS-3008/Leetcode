44303a3bf1ae8acad1e0561c85e26c900f0bcfdf
TEJAS-3008 <186479992+TEJAS-3008@users.noreply.github.com>
2026-08-06 18:32:16 +0530

    1486.XorOperationInAnArray.py

---
+++ 1486.XorOperationInAnArray.py
@@ -0,0 +1,9 @@
+class Solution:
+    def xorOperation(self, n: int, start: int) -> int:
+        ans = 0
+        for i in range(0, n):
+            num = start + 2 * i
+            ans = ans ^ num
+
+        return ans
+
